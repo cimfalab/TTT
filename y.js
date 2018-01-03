23e9ed3cdec502583c9ff821bfaec38888a1974e
Kim Kangho <cimfalab@gmail.com>
2018-01-03 13:51:36 +0900

    Create y.js

---
+++ y.js
@@ -0,0 +1,21 @@
+function aaaa() {
+    var pChangeMessageList = {};
+    var param = typeof pChangeMessageList == "Array" ? pChangeMessageList.join('|') : pChangeMessageList;
+    console.log(param);
+
+    (function updateZkConfigs(configs) {
+      var zks = this.getZkServerHosts();
+      var portValue = configs['zoo.cfg'] && Em.get(configs['zoo.cfg'], 'clientPort');
+      var zkPort = typeof portValue === 'udefined' ? '2181' : portValue;
+      var zksWithPort = this.concatZkNames(zks, zkPort);
+      this.setZKConfigs(configs, zksWithPort, zks);
+    })({});
+}
+
+function a2() {
+    if ($.browser.msie == true) {
+        var target = url + "userName" + userName; // 1)
+        target = url.replace(/\.|\?|\&|\/|\=|\:|\-|\s/gi,""); // 2)
+        return target;
+    }
+}
