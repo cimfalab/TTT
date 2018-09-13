f1e11204c545760e3d6abeb53eb3e82c06cd2b5d
Kim Kangho <cimfalab@gmail.com>
2018-09-13 21:38:55 +0900

    pg-native

--- react.js
+++ react.js
@@ -3,6 +3,6 @@ import React from 'react';
 
 class Hello extends React.Component {
     render() {
-        return <textarea>Hi</textarea>; // REACT_DEPRECATED_DOM_ELEMENT_PROP alarm because child node is set on 'textarea' element.
+        return <textarea></textarea>; // REACT_DEPRECATED_DOM_ELEMENT_PROP alarm because child node is set on 'textarea' element.
     }
 }
