7b016af1db184277e9f8eff408b30ae14a97f5bf
Kim Kangho <cimfalab@gmail.com>
2019-03-27 15:35:25 +0900

    Create rrr.js

---
+++ rrr.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function test() {
+  <Hello name="foo" />;
+}
+
+function test1() {
+  <Hello name="foo" />;
+}
+
+function test2() {
+  <Hello name="foo" />;
+}
+
+function test3() {
+  <Hello name="foo" />;
+}
