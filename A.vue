3ae77e9cf21dd85b4be4e38d9b501b788232abf7
Kim Kangho <cimfalab@gmail.com>
2020-09-01 16:01:52 +0900

    Update A.vue

--- A.vue
+++ A.vue
@@ -1,24 +1,7 @@
-<template>
-  <div>
-    <input v-model.ltrim="msg"> <!-- VUE_BAD_DIRECTIVE_FORMAT alarm because 'ltrim' modifier is not supported for 'v-model' directive. -->
-    <div v-if="shouldHi">Hi</div>
-    <div v-else="shouldBye">Bye</div> <!-- VUE_BAD_DIRECTIVE_FORMAT alarm because value is not needed for 'v-else' directive. -->
-  </div>
-</template>
-<script>
-  export default {
-    data() {
-      return { msg: 'hello world' };
-    },
-    props: {
-      shouldHi: {
-        type: Boolean,
-        default: true
-      },
-      shouldBye: {
-        type: Boolean,
-        default: false
-      }
-    }
-  }
-</script>
+    <template>
+      <div>
+        <div v-for="i in items">
+          {{ items[i] /* VUE_MISUSED_V_FOR_VAR alarm */ }}
+        </div>
+      </div>
+    </template>
